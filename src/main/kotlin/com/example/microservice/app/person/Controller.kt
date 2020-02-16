f788b2aa5626f5a9f2bd545adbf7227164d75a1e
Mateusz Warzyński <mathew.warzynski@gmail.com>
2020-02-16 04:32:43 +0100

    feat: Introduce Service layer

--- src/main/kotlin/com/example/microservice/app/person/Controller.kt
+++ src/main/kotlin/com/example/microservice/app/person/Controller.kt
@@ -3,23 +3,26 @@ package com.example.microservice.app.person
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.web.PageableDefault
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 import java.util.*
 
-
 @RestController
 @RequestMapping("/people")
-class Controller(private val repository: PersonRepository) {
+class Controller(private val repository: PersonService) {
 
     @GetMapping("/{id}")
-    fun findById(@PathVariable id: String): Optional<Person> = repository.findById(id)
+    fun getById(@PathVariable id: String): Optional<Person> = repository.getById(id)
 
     @GetMapping
-    fun findAll(@PageableDefault pageable: Pageable): Page<Person> = repository.findAll(pageable)
+    fun getAll(@PageableDefault pageable: Pageable): Page<Person> = repository.getAll(pageable)
 
     @PostMapping
-    fun add(@RequestBody person: Person): Person = repository.save(person)
+    fun create(@RequestBody person: Person): Person = repository.create(person)
+
+    @PutMapping("/{id}")
+    fun update(@PathVariable id: String, @RequestBody person: Person): Person = repository.update(id, person).get()
 
     @DeleteMapping("/{id}")
-    fun delete(@PathVariable id: String) = repository.deleteById(id)
+    fun delete(@PathVariable id: String) = repository.delete(id)
 }
---
+++ src/main/kotlin/com/example/microservice/app/person/Service.kt
@@ -0,0 +1,28 @@
+package com.example.microservice.app.person
+
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.stereotype.Service
+import java.util.*
+
+interface PersonService {
+    fun getAll(pageable: Pageable): Page<Person>
+    fun getById(id: String): Optional<Person>
+    fun create(person: Person): Person
+    fun update(id: String, person: Person): Optional<Person>
+    fun delete(id: String)
+}
+
+@Service
+class PersonServiceImpl(
+        private val repository: PersonRepository
+) : PersonService {
+    override fun getAll(pageable: Pageable): Page<Person> = repository.findAll(pageable)
+    override fun getById(id: String): Optional<Person> = repository.findById(id)
+    override fun create(person: Person): Person = repository.save(person)
+    override fun update(id: String, person: Person): Optional<Person> = getById(id).map {
+        it.name = person.name
+        repository.save(it)
+    }
+    override fun delete(id: String) = repository.deleteById(id)
+}
